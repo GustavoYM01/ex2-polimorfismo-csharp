5e7d9e6ce24c98d0aa9e5e71809abae0e2817d71
GustavoYM01 <gumatimoto@hotmail.com>
2022-02-12 17:20:51 -0300

    First commit - added files

---
+++ polimorf-2/Entities/UsedProduct.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace polimorf_2.Entities
+{
+    class UsedProduct : Product
+    {
+        CultureInfo CI = CultureInfo.InvariantCulture;
+
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price, 
+            DateTime manufactureDate) : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+                + " (used) $ "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + " (Manufacture date: "
+                + ManufactureDate.ToString("dd/MM/yyyy")
+                + ")";
+        }
+
+    }
+}
